063b028c015c2b55df891b08bda760408e20b6e9
Оксана Джурик <oksi2196@outlook.com>
2017-04-28 12:31:43 +0300

    Add project files.

---
+++ BlogMVC/Models/DAL/BlogContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace BlogMVC.Models.DAL
+{
+    public class BlogContext:DbContext
+    {
+        public BlogContext():base("BlogConnection")
+        {
+
+        }
+        public DbSet<Post> Posts { get; set; }
+    }
+}
---
+++ BlogMVC/Models/Post.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BlogMVC.Models
+{
+    public class Post
+    {
+        public int ID { get; set; }
+        [Display(Name ="Заголовок")]
+        public string Title { get; set; }
+        [Display(Name ="Текст")]
+        public string Text { get; set; }
+        public DateTime Time { get; set; }
+    }
+}
